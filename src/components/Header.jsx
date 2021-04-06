be0c0c228e5f2b3ef8a4518ca7e22b71bf48b505
aleksandrshliakhovoi <aleksandrshliakhovoi@gmail.com>
2021-04-07 02:45:13 +0300

    Divide on components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/How_to_use_icon.svg" />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+      <img src="https://creativepro.com/wp-content/uploads/2019/05/imagetext01.jpg" className="header-img"/>
+      </div>
+      <div>
+        ava + desc
+        <img src="https://www.mirserpen.ru/media/2014/11/210-450x297.jpg" alt=""/>
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
+
